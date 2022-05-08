568948bb58cd4e52999cbd2d144df94adce560a0
Pandoras <saya@saya.ac.cn>
2022-05-08 10:10:04 +0800

    补交

---
+++ src/main/java/ac/cn/saya/flink/table/Example.java
@@ -0,0 +1,58 @@
+package ac.cn.saya.flink.table;
+
+import ac.cn.saya.flink.entity.SensorReading;
+import org.apache.calcite.schema.StreamableTable;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
+
+/**
+ * @Title: Example
+ * @ProjectName flink-util
+ * @Description: TODO
+ * @Author saya
+ * @Date: 2022/1/31 16:04
+ * @Description:
+ */
+
+public class Example{
+    public static void main(String[] args) throws Exception {
+        // 1、创建执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        // 2、从文件中读取
+        DataStream<String> sourceStream = env.readTextFile("/Users/saya/project/java/flink-util/src/main/resources/sensor-sample.txt");
+        // 3、转换成实体实体
+        DataStream<SensorReading> dataStream = sourceStream.map(new MapFunction<String, SensorReading>() {
+            @Override
+            public SensorReading map(String line) throws Exception {
+                String[] fileds = line.split(",");
+                return new SensorReading(fileds[0],Long.parseLong(fileds[1]),Double.parseDouble(fileds[2]));
+            }
+        });
+        // 4、创建表环境
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+
+        // 5、基于流创建一张表
+        Table dataTable = tableEnv.fromDataStream(dataStream);
+
+        // 6、调用table api进行转换操作
+        Table resultTable = dataTable.select("id,temperature").where("id = 'sensor_1'");
+
+        // 通过sql进行查询
+        // 注册表
+        tableEnv.createTemporaryView("sensor",dataTable);
+        String sql = "select id,temperature from sensor where id = 'sensor_1'";
+        Table resultSqlTable = tableEnv.sqlQuery(sql);
+
+        // 7、打印输出
+        tableEnv.toAppendStream(resultTable, Row.class).print("orm");
+        tableEnv.toAppendStream(resultSqlTable,Row.class).print("sql");
+
+        // 8、提交执行
+        env.execute();
+    }
+}
---
+++ src/main/java/ac/cn/saya/flink/table/Test_KafkaPipeline.java
@@ -0,0 +1,62 @@
+package ac.cn.saya.flink.table;
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.java.StreamTableEnvironment;
+import org.apache.flink.table.descriptors.Csv;
+import org.apache.flink.table.descriptors.Kafka;
+import org.apache.flink.table.descriptors.Schema;
+
+import java.util.Properties;
+
+/**
+ * @Title: Test_KafkaPipeline
+ * @ProjectName flink-util
+ * @Description: TODO
+ * @Author saya
+ * @Date: 2022/1/31 20:54
+ * @Description:
+ */
+
+public class Test_KafkaPipeline{
+    public static void main(String[] args) throws Exception {
+        // 创建执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+        tableEnv.connect(new Kafka().version("0.11")
+        .topic("sensor")
+        .property("zookeeper.connect","127.0.0.1:2181")
+        .property("bootstrap.servers","127.0.0.1:9092"))
+        .withFormat(new Csv())
+        .withSchema(new Schema()
+        .field("id", DataTypes.STRING())
+        .field("timestamp",DataTypes.BIGINT())
+        .field("temperature",DataTypes.DOUBLE()))
+        .createTemporaryTable("inputTable");
+
+        Table sensorTable = tableEnv.from("inputTable");
+        Table resultTable = sensorTable.select("id,temperature").filter("id === 'sensor_1'");
+
+        Table avgTable = sensorTable.groupBy("id").select("id,id.count as count,temperature as avgTemp");
+        tableEnv.connect(new Kafka().version("0.11")
+                .topic("sink-test")
+                .property("zookeeper.connect","127.0.0.1:2181")
+                .property("bootstrap.servers","127.0.0.1:9092"))
+                .withFormat(new Csv())
+                .withSchema(new Schema()
+                        .field("id", DataTypes.STRING())
+                        .field("timestamp",DataTypes.BIGINT())
+                        .field("temperature",DataTypes.DOUBLE()))
+                .createTemporaryTable("outputTable");
+
+        resultTable.insertInto("outputTable");
+
+        env.execute();
+    }
+}
